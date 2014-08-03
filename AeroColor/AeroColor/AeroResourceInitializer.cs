c4d5f25ab565cac03f0e74696e7ae0335d9c8f94
ffMathy <mathias.lorenzen@live.com>
2014-08-03 16:20:19 +0200

    basic structure.

---
+++ AeroColor/AeroColor/AeroResourceInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AeroColor
+{
+    public static class AeroResourceInitializer
+    {
+        public static void Initialize(Application application)
+        {
+
+        }
+    }
+}
